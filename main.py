6217b34cf9da6bfd1969cb8f05ccc135387e7b42
Kobe Li <likobe23@gmail.com>
2023-11-03 11:36:09 -0700

    Remove comment

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from json import dumps as json_dumps
-
 from models import Property
-
 
 properties = {}
 
 for i in range(0, 150):
     property = Property()
     properties[property.mls] = property.to_dict()
-
-# print(json_dumps(properties, indent=4))
 
 with open("output.json", "w") as f:
     f.write(json_dumps(properties, indent=4))
