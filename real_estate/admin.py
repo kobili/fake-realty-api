dcd85f3ca0bede09c3aae2f39cc3d778dde62d7f
Kobe Li <likobe23@gmail.com>
2023-11-12 13:24:13 -0800

    Add admin action to generate properties

--- real_estate/admin.py
+++ real_estate/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import Address, Property
+from .fake_factories import Property as FakeProperty
 
 
 class AddressAdmin(admin.ModelAdmin):
@@ -25,6 +26,25 @@ class PropertyAdmin(admin.ModelAdmin):
         "bathrooms",
         "title",
     ]
+
+    fields = [
+        "mls",
+        ("monthly_rent", "purchase_price"),
+        ("square_footage", "bedrooms", "bathrooms"),
+        "title",
+        "amenities",
+        "address",
+    ]
+
+    actions = ["generate_new_property"]
+
+    @admin.action(description="Generate a new property listing")
+    def generate_new_property(self, request, queryset):
+        new_property = FakeProperty().to_dict()
+        new_address = new_property.pop("address")
+
+        addr = Address.objects.create(**new_address)
+        Property.objects.create(address=addr, **new_property)
 
 
 # Register your models here.
